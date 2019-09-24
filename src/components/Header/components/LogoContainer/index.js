428e80fe1a3cc4969dd7787258832c8b9225bdbb
daniel31737 <vanthao31737@gmail.com>
2019-09-24 17:58:23 +0700

    show skip content

---
+++ src/components/Header/components/LogoContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './style.scss';
+export default class LogoContainer extends Component {
+    render() {
+        return (
+            <a href="/" className="logo">
+                <div className="logo-container"></div>
+            </a>
+        )
+    }
+}
